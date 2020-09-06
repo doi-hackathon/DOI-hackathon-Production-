ab4d151e9b577304595f15b9a114ace2e6caf6bc
Student Name <rexsutton2@gmail.cogit>
2020-09-06 13:23:32 -0500

    added links to icons for facebook, twitter and instagram for home page footer

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -59,7 +59,7 @@ jQuery(function ($) {
 
    if ($('.countdown').length > 0) {
       $(".countdown").jCounter({
-         date: '15 September 2020 12:00:00',
+         date: '21 September 2020 12:00:00',
          fallback: function () {
             console.log("count finished!")
          }
