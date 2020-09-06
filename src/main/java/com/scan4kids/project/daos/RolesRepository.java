50dc3da7c23deb33f8dc024d9697b291d24eec88
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-06 12:27:03 -0500

    feat: added join column for roles.

--- src/main/java/com/scan4kids/project/daos/RolesRepository.java
+++ src/main/java/com/scan4kids/project/daos/RolesRepository.java
@@ -1,7 +1,8 @@
-//package com.scan4kids.project.daos;
-//
-//import com.scan4kids.project.models.Score;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RolesRepository extends JpaRepository<Score, Long> {
-//}
+package com.scan4kids.project.daos;
+
+import com.scan4kids.project.models.Role;
+import com.scan4kids.project.models.Score;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesRepository extends JpaRepository<Role, Long> {
+}
