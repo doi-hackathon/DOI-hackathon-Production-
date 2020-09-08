77e42aae2081ae68e48d5278242efcb362bc17b0
Student Name <rexsutton2@gmail.cogit>
2020-09-08 13:48:30 -0500

    removed anchor tag from navbar doi logo

--- src/main/java/com/scan4kids/project/controllers/IndexController.java
+++ src/main/java/com/scan4kids/project/controllers/IndexController.java
@@ -1,6 +1,6 @@
 package com.scan4kids.project.controllers;
 
-import org.springframework.stereotype.Controller;
+import   org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
