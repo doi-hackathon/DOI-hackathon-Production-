9f44a48350173389167874bf8bcc80fa399f75c0
Ry Sutton <ryancesutton@gmail.com>
2020-09-13 16:05:58 -0500

    hide submissions portion of dash once user has submitted

--- src/main/java/com/scan4kids/project/daos/SubmissionsRepository.java
+++ src/main/java/com/scan4kids/project/daos/SubmissionsRepository.java
@@ -1,7 +1,11 @@
 package com.scan4kids.project.daos;
 
 import com.scan4kids.project.models.Submission;
+import com.scan4kids.project.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubmissionsRepository extends JpaRepository<Submission, Long> {
+
+    Submission findSubmissionsByOwner(User owner);
+
 }
