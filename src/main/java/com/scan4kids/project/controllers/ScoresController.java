e16da3cbc2dab19a3b51f1d39c9a10472ff0b1e8
Student Name <rexsutton2@gmail.cogit>
2020-09-27 21:02:48 -0500

    set up submission view structure and correct verbiage in judging instructions

--- src/main/java/com/scan4kids/project/controllers/ScoresController.java
+++ src/main/java/com/scan4kids/project/controllers/ScoresController.java
@@ -1,8 +1,14 @@
 package com.scan4kids.project.controllers;
 
 
+import com.scan4kids.project.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class ScoresController {
@@ -11,6 +17,17 @@ public class ScoresController {
     public String showScoreForm() {
         return "scores/scores";
     }
+
+//    @GetMapping("/score/{id}")
+//    public String show(@PathVariable long id, Model model){
+//        Event event = eventsDao.getOne(id);
+//        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        List<UsersEvents> usersEvents = usersEventsDao.findAllByUserId(currentUser.getId());
+//        byte attendeeCount = usersEventsDao.countByEventAndUser(event, currentUser);
+//        List<UsersEvents> volunteeredEventsList = usersEventsDao.findAllVolunteeredEvents(id);
+//
+//        return "events/show";
+//    }
 
 
 }
