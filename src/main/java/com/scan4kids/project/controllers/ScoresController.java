b81b83e77328e5b1a38b8abb0ed326b0ce92b92f
Student Name <rexsutton2@gmail.cogit>
2020-09-27 19:24:13 -0500

    added scores controller and directory to templates

---
+++ src/main/java/com/scan4kids/project/controllers/ScoresController.java
@@ -0,0 +1,16 @@
+package com.scan4kids.project.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ScoresController {
+
+    @GetMapping("/score")
+    public String showScoreForm() {
+        return "users/scores";
+    }
+
+
+}
