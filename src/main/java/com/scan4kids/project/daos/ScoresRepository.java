a8e677ae29768eabcbc64adbd6916ddeffad2072
Ry Sutton <ryancesutton@gmail.com>
2020-09-29 16:59:59 -0500

    started working on hide button

--- src/main/java/com/scan4kids/project/daos/ScoresRepository.java
+++ src/main/java/com/scan4kids/project/daos/ScoresRepository.java
@@ -1,7 +1,13 @@
 package com.scan4kids.project.daos;
 
 import com.scan4kids.project.models.Score;
+import com.scan4kids.project.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScoresRepository extends JpaRepository<Score, Long> {
+
+    List<Score> findAllByJudge(User judge);
+
 }
